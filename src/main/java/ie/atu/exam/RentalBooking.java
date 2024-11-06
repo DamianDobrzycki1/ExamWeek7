cd77a33ffe5f2507510cfd199419d560dcb561e1
G00419511@atu.ie <G00419511@atu.e>
2024-11-06 14:30:07 +0000

    added messages into RentalBooking and added RestController, RequestMapping, GetMapping and PostMapping into rentals java file

--- src/main/java/ie/atu/exam/RentalBooking.java
+++ src/main/java/ie/atu/exam/RentalBooking.java
@@ -13,11 +13,11 @@ import lombok.NoArgsConstructor;
 
 public class RentalBooking {
 
-    @NotBlank private String rentalCode;
-    @NotBlank private String carModel;
-    @NotBlank private String name;
-    @Email private String email;
-    @PositiveOrZero private int StartDate;
-    @PositiveOrZero private int EndDate;
-    @PositiveOrZero private int dailyRate;
+    @NotBlank (message = "Please enter an rentalCode") private String rentalCode;
+    @NotBlank (message = "Please enter an carModel") private String carModel;
+    @NotBlank (message = "Please enter an name") private String name;
+    @Email (message = "Please enter an Email") private String email;
+    @PositiveOrZero (message = "Please enter an StartDate") private int StartDate;
+    @PositiveOrZero (message = "Please enter an EndDate") private int EndDate;
+    @PositiveOrZero (message = "Please enter an dailyRate") private int dailyRate;
 }
